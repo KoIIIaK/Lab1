7223a190556086b1dd1dee4dab16f4f5772299fc
KoT <egor_solovev2001@mail.ru>
2019-02-25 17:27:15 +0300

    commit2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    class Global
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int YearOpen { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine("Название " + Name + "Адрес " + Address + "Год открытия " + YearOpen);
+        }
+
+        public void Read()
+        {
+            Console.WriteLine("Введите название библиотеки");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите адрес библиотеки");
+            Address = Console.ReadLine();
+            Console.WriteLine("Введите год открытия библиотеки");
+            YearOpen = Convert.ToInt16(Console.ReadLine());
+        }
+
+        public Global()
+        {
+            Name = "";
+            Address = "";
+            YearOpen = 0;
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
